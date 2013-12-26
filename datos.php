10f81ecf1f464c1ee829fbaac752685e07781439
Álvaro Ferreira <alvaroferreira78@gmail.com>
2013-12-26 17:48:18 +0100

    Código base

---
+++ datos.php
@@ -0,0 +1,60 @@
+﻿<?php
+header("Content-type: text/javascript");
+function DegToRad($anguloDeg){
+	return ($anguloDeg*pi())/180;
+}
+function Distancia($lat1,$lon1,$lat2,$lon2)
+{
+	$R = 6371;
+	$dLat = DegToRad($lat2-$lat1);
+	$dLon = DegToRad($lon2-$lon1);
+	$lat1 = DegToRad($lat1);
+	$lat2 = DegToRad($lat2);
+
+	$a = sin($dLat/2) * sin($dLat/2) +
+			sin($dLon/2) * sin($dLon/2) * cos($lat1) * cos($lat2); 
+	$c = 2 * atan2(sqrt($a), sqrt(1-$a)); 
+	$d = $R * $c;
+
+		return $d;
+}
+
+if (file_exists('practica.xml')) {
+    $datos = simplexml_load_file('practica.xml');
+ 
+    //print_r($datos);
+} else {
+    exit('Error abriendo practica.xml.');
+}
+//var_dump($datos);
+$arraydatos=array();
+$contador=0;
+
+foreach ($datos->trk->trkseg[0]->trkpt as $valor) {
+	$latitud = strval($valor->attributes()->lat);
+	$longitud = strval($valor->attributes()->lon);
+	
+	$elevacion = strval($valor->ele);
+	$tiempo = strval($valor->time);
+	if($contador==0)
+	{
+		$distancia=0;
+		$lat=$latitud;
+		$lon=$longitud;
+	}
+	else
+	{
+		$distancia += Distancia($latitud, $longitud, $lat, $lon);
+		$lat=$latitud;
+		$lon=$longitud;
+	}
+	if($elevacion != '')
+	{
+		$arraydatos[] = array('latitud'=>$latitud, 'longitud'=>$longitud, 'elevacion'=>$elevacion, 'distancia'=>$distancia, 'tiempo'=>$tiempo);
+	}
+	$contador++;
+}
+$datosjson = json_encode($arraydatos);
+echo $datosjson;
+
+?>
---
+++ js/codigo.js
@@ -0,0 +1,120 @@
+﻿function DifHoras(laHora1, laHora2){
+	var hms1 = laHora1.split(':');
+	var hms2 = laHora2.split(':');
+	
+	if(hms2[2]>hms1[2])
+	{
+		return hms2[2]-hms1[2];
+	}
+	else
+	{
+		return parseInt(hms2[2]) + parseInt((60 - hms1[2]));
+	}
+}
+
+function GetDiftime(tiempo){
+	//console.log(tiempo);
+	//console.log(DifHoras(tiempo[0],tiempo[1]));
+	var tiempoacumulado = [];
+	var pivote = 0;
+	$.each( tiempo, function( key, value ) {
+		if(key==0)
+		{
+			tiempoacumulado.push(0);
+		}
+		else
+		{
+			tiempoacumulado.push(DifHoras(pivote, value));
+			//alert(pivote +' '+value);
+		}
+		pivote = value;
+	});
+	
+	var acum = 0;
+	var tiemposumado = [];
+	tiemposumado.push(acum);
+	$.each( tiempoacumulado, function( key, value ) {
+		if(key!=0)
+		{
+			acum = acum + value;
+			tiemposumado.push(acum);
+		}
+	});
+	return tiemposumado;
+}
+
+function GraficaPuntos(recorrido, SumTime, salto){
+
+	var last_element = recorrido[recorrido.length - 1];
+	var elfor = Math.floor(last_element);
+	
+	var last_time = SumTime[SumTime.length - 1];
+	var saltosRec = [];
+	var saltosTim = [];
+	
+	var paso = 0;
+	
+	$.each( recorrido, function( key, value ) {
+		if(value >= paso)
+		{
+			saltosRec.push(paso);
+			saltosTim.push(parseInt(SumTime[key])/60);
+			paso=paso+salto;
+			//console.log(value+' '+SumTime[key]);
+		}
+	});
+	saltosRec.push(last_element);
+	saltosTim.push(parseInt(last_time)/60);
+	
+	/* --- CÁLCULO DE LA LINEA IDEAL A 5 MIN/KM --- */
+	
+	var saltosTimIdeal = [];
+	$.each( saltosRec, function( key, value ) {
+	
+			saltosTimIdeal.push((parseFloat(value)*300)/60);
+	});
+	/* --- ------------------------------------ --- */
+	$('#container').highcharts({
+        chart: {
+			//height: 200
+        },
+        xAxis: {
+            categories: saltosRec,
+			title: {
+                enabled: true,
+                text: '<b>Distancia (km)</b>',
+                style: {
+                    fontWeight: 'normal'
+                }
+            }
+        },
+		yAxis: {
+			title: {
+                enabled: true,
+                text: '<b>Tiempo (min)</b>',
+                style: {
+                    fontWeight: 'normal'
+                }
+            }
+        },
+		title: {
+            text: 'Tiempo - Distancia'  
+        },
+        plotOptions: {
+            series: {
+                color: '#FF0000'
+            }
+        },
+        series: [{
+			type: 'line',
+            data: saltosTim,
+			name: 'Real'
+			},
+			{
+			type: 'line',
+            data: saltosTimIdeal,
+			name: 'Ideal',
+			color: 'blue'
+        }]
+    });
+}
