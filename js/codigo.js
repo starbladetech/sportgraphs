d6e4ad5eaba53fb6dcc1df50e954198aa64dd521
Álvaro Ferreira <alvaroferreira78@gmail.com>
2013-12-26 20:10:40 +0100

    Mejoras en todo

--- js/codigo.js
+++ js/codigo.js
@@ -43,6 +43,30 @@ function GetDiftime(tiempo){
 	return tiemposumado;
 }
 
+function FormateoTime(milisegundos){
+	var horas = 0;
+	var minutos = 0;
+	var segundos = 0;
+	
+	var cociente = Math.floor(parseInt(milisegundos) / 3600000);
+	var resto = parseInt(milisegundos) % 3600000;
+	
+	if(cociente>1)
+	{
+		horas = cociente;
+	}
+		cociente = Math.floor(resto / 60000);
+		resto = resto % 60000;
+		
+		if(cociente>1)
+		{
+			minutos = cociente;
+		}
+			segundos = resto/1000;
+	
+		return horas+'h:'+minutos+'m:'+segundos+'s';
+}
+
 function GraficaPuntos(recorrido, SumTime, salto){
 
 	var last_element = recorrido[recorrido.length - 1];
@@ -58,28 +82,45 @@ function GraficaPuntos(recorrido, SumTime, salto){
 		if(value >= paso)
 		{
 			saltosRec.push(paso);
-			saltosTim.push(parseInt(SumTime[key])/60);
+			saltosTim.push(parseInt(SumTime[key]));
 			paso=paso+salto;
 			//console.log(value+' '+SumTime[key]);
 		}
 	});
 	saltosRec.push(last_element);
-	saltosTim.push(parseInt(last_time)/60);
+	saltosTim.push(parseInt(last_time));
 	
 	/* --- CÁLCULO DE LA LINEA IDEAL A 5 MIN/KM --- */
 	
 	var saltosTimIdeal = [];
 	$.each( saltosRec, function( key, value ) {
 	
-			saltosTimIdeal.push((parseFloat(value)*300)/60);
+			saltosTimIdeal.push((parseFloat(value)*300));
 	});
+	
+	/* --- PREPARAR LOS DATOS PARA HIGHCHARTS ---*/
+	var datalin=new Array(saltosRec.length);
+	var datalinIdeal=new Array(saltosRec.length);
+	
+	for (var i = 0; i < saltosRec.length; i++) {
+		datalin[i] = new Array(2);
+		datalinIdeal[i] = new Array(2);
+	}
+	for(var cursor=0; cursor < saltosRec.length; cursor++)
+	{
+		datalin[cursor][0] = saltosRec[cursor];
+		datalin[cursor][1] = saltosTim[cursor]*1000+Date.UTC(2013,12,14,17,18,47);
+		
+		datalinIdeal[cursor][0] = saltosRec[cursor];
+		datalinIdeal[cursor][1] = saltosTimIdeal[cursor]*1000+Date.UTC(2013,12,14,17,18,47);
+	}
 	/* --- ------------------------------------ --- */
 	$('#container').highcharts({
         chart: {
 			//height: 200
         },
         xAxis: {
-            categories: saltosRec,
+            //categories: saltosRec,
 			title: {
                 enabled: true,
                 text: '<b>Distancia (km)</b>',
@@ -91,10 +132,14 @@ function GraficaPuntos(recorrido, SumTime, salto){
 		yAxis: {
 			title: {
                 enabled: true,
-                text: '<b>Tiempo (min)</b>',
+                text: '<b>Tiempo</b>',
                 style: {
                     fontWeight: 'normal'
                 }
+            },
+			type: 'datetime',
+            dateTimeLabelFormats: {
+                second: '%H:%M:%S',
             }
         },
 		title: {
@@ -105,14 +150,21 @@ function GraficaPuntos(recorrido, SumTime, salto){
                 color: '#FF0000'
             }
         },
+		tooltip: {
+            formatter: function() {
+			
+                return 'El tiempo para <b>'+ this.x +
+                    ' km</b> es <b>'+ FormateoTime(this.y-Date.UTC(2013,12,14,17,18,47)) +'</b>';
+            }
+        },
         series: [{
 			type: 'line',
-            data: saltosTim,
+            data: datalin,
 			name: 'Real'
 			},
 			{
 			type: 'line',
-            data: saltosTimIdeal,
+            data: datalinIdeal,
 			name: 'Ideal',
 			color: 'blue'
         }]
